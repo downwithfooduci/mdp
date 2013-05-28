0757a83c22a32a8167ae54c075fb28604e585c03
Christopher Dinh <christod@uci.edu>
2013-05-27 20:00:07 -0700

    Added GameConfig component that forces landscape mode on iPad

---
+++ Assets/Scripts/Managers/GameConfig.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class GameConfig : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+	}
+}
