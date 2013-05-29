0a51732dea2d4562562604bdb00bbeebe857943a
Stanislav Listopad <staslist@yahoo.com>
2013-05-28 19:20:51 -0700

    Merge of both games.

--- Assets/Scripts/Demos/Buttons.cs
+++ Assets/Scripts/Demos/Buttons.cs
@@ -12,6 +12,7 @@ public class Buttons : MonoBehaviour {
 	public GameObject ColorPrefeb;
 	public static bool EnzymesExist = false;
 	private Color[] colorChoice = {Color.green, Color.white, Color.yellow, Color.blue, Color.red};
+	public GUIStyle style;
 	void OnGUI(){
 		/*if(GUI.Button(new Rect(Screen.width - 100,Screen.height -50 ,100,40), "Generator")){
 		
@@ -20,6 +21,12 @@ public class Buttons : MonoBehaviour {
 			ObjectGenerator.particles[0].renderer.material.color = Color.green;
 		}*/
 		//NEED TO FIX COLORING PROBLEMS
+		
+		string toDisplay = (90 - Time.time) + "";
+		GUI.Label (new Rect(20,20,300,300), toDisplay, style);
+		if(Time.time >= 90)
+			Time.timeScale = 0;
+		
 		
 		if (GUI.Button(new Rect(Screen.width - 100,Screen.height -100 ,100,40), "RedEnzyme")) {
 			if(!EnzymesExist){
