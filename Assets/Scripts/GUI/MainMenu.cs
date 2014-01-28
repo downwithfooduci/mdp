721e4e0611b3915828d0c64ff6de540e18e3a2cc
Chris Berizko <cberizko@gmail.com>
2014-01-27 17:51:35 -0800

    added main menu texture

--- Assets/Scripts/GUI/MainMenu.cs
+++ Assets/Scripts/GUI/MainMenu.cs
@@ -2,23 +2,24 @@ using UnityEngine;
 using System.Collections;
 
 public class MainMenu : MonoBehaviour {
-
+	public Texture background;
 	void OnGUI()
     {
 		Debug.Log(Screen.width);
-		if (GUI.Button(new Rect(Screen.width * 0.4f, Screen.height * 0.4f, 
+		GUI.DrawTexture(new Rect(0,0,Screen.width, Screen.height), background);
+		if (GUI.Button(new Rect(Screen.width * 0.15f, Screen.height * 0.8f, 
 		                        Screen.width * 0.2f, Screen.height * 0.1f), "Enzyme Game"))
         {
             Application.LoadLevel("Enzyme");
         }
 
-		if (GUI.Button(new Rect(Screen.width * 0.4f, Screen.height * 0.55f,
+		if (GUI.Button(new Rect(Screen.width * 0.4f, Screen.height * 0.8f,
 		                        Screen.width * 0.2f, Screen.height * 0.1f), "Small Intestine Game"))
         {
             Application.LoadLevel("SmallIntestine");
         }
 		
-		if (GUI.Button(new Rect(Screen.width * 0.4f, Screen.height * 0.7f, 
+		if (GUI.Button(new Rect(Screen.width * 0.65f, Screen.height * 0.8f, 
 		                        Screen.width * 0.2f, Screen.height * 0.1f), "Pinch to Zoom Test"))
         {
             Application.LoadLevel("pinch");
