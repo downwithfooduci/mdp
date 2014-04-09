8a658b3d7f668359723ced3758c20f229b54c067
Chris Berizko <cberizko@gmail.com>
2014-04-08 22:45:49 -0700

    added in tongue

---
+++ Assets/MoveTongue.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTongue : MonoBehaviour {
+	public GameObject wall;
+	float moved, originalHeight, maxMove;
+	// Use this for initialization
+	void Start () {
+		originalHeight = 3.48f;
+		maxMove = .4f;
+		moved = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.touches.Length == 1)
+		{
+			Touch touch = Input.touches[0];
+			Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+			touchPos = new Vector3(touchPos.x, touchPos.y, transform.position.z);
+			if(Vector3.Distance(touchPos, transform.position) < .4)
+			{
+				moved = Mathf.Clamp(moved + 
+				                    touch.deltaPosition.y / 20f, 0, maxMove);
+			}
+		}
+		if(Input.GetKey(KeyCode.S))
+		{
+			moved = Mathf.Clamp(moved - .4f * Time.deltaTime, 0, maxMove);
+		}
+		else if(Input.GetKey(KeyCode.W))
+		{
+			moved = Mathf.Clamp(moved + .4f * Time.deltaTime, 0, maxMove);
+		}
+		transform.position = new Vector3(transform.position.x, originalHeight + moved, transform.position.z);
+		if(moved >= maxMove - .05f)
+		{
+			wall.transform.position = new Vector3(wall.transform.position.x, 0, wall.transform.position.z);
+		}
+		else
+		{
+			wall.transform.position = new Vector3(wall.transform.position.x, 3.5f, wall.transform.position.z);
+		}
+	}
+}
