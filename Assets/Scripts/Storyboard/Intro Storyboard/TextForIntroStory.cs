3cc1e972212a5a3dd0cb307dac5cac8998735ff1
Chantal <sakuya3834@gmail.com>
2014-06-03 10:43:44 -0700

    started adding text for subtitles

---
+++ Assets/Scripts/Storyboard/Intro Storyboard/TextForIntroStory.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextForIntroStory : MonoBehaviour 
+{
+	private string[] text;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
