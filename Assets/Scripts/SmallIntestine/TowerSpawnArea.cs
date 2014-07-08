676ddf3e199fbe5996b1ca8a18bf0f71de95d034
Chantal <sakuya3834@gmail.com>
2014-07-08 00:04:23 -0700

    fixed a bug with placing towers, changed iOS loading screen default orientation

--- Assets/Scripts/SmallIntestine/TowerSpawnArea.cs
+++ Assets/Scripts/SmallIntestine/TowerSpawnArea.cs
@@ -3,21 +3,41 @@ using System.Collections;
 
 public class TowerSpawnArea : MonoBehaviour 
 {
-	private TowerSpawnerTutorial towerSpawner;
+	private TowerSpawner towerSpawner;
+	private TowerSpawnerTutorial towerSpawnerTutorial;
 
 	void Start () 
 	{
-		towerSpawner = FindObjectOfType(typeof(TowerSpawnerTutorial)) as TowerSpawnerTutorial;
+		if (Application.loadedLevelName == "SmallIntestineTutorial")
+		{
+			towerSpawnerTutorial = FindObjectOfType(typeof(TowerSpawnerTutorial)) as TowerSpawnerTutorial;
+		} else
+		{
+			towerSpawner = FindObjectOfType(typeof(TowerSpawner)) as TowerSpawner;
+		}
 	}
 
     void OnMouseOver()
     {
-        towerSpawner.IsMouseOverWall = true;
-		towerSpawner.wall = gameObject;
+		if (Application.loadedLevelName == "SmallIntestineTutorial")
+		{
+			towerSpawnerTutorial.IsMouseOverWall = true;
+			towerSpawnerTutorial.wall = gameObject;
+		} else
+		{
+	        towerSpawner.IsMouseOverWall = true;
+			towerSpawner.wall = gameObject;
+		}
     }
 
     void OnMouseExit()
     {
-        towerSpawner.IsMouseOverWall = false;
+		if (Application.loadedLevelName == "SmallIntestineTutorial")
+		{
+			towerSpawnerTutorial.IsMouseOverWall = false;
+		} else
+		{
+			towerSpawner.IsMouseOverWall = false;
+		}
     }
 }
