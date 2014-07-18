d2f65b8ea1374c70e9d7df046830d6c269260819
Chantal <sakuya3834@gmail.com>
2014-07-18 16:04:24 -0700

    added in prefabs for shader glow masks and put places on the walls to spawn either a shader or spotlight (whatever we use) along with 2 control scripts

---
+++ Assets/GlowSegment.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlowSegment : MonoBehaviour 
+{
+	public GameObject cube;
+	public Material glowMask;
+	private GameObject instantiatedCube;
+
+	public float dieTime;
+	private float elapsedTime;
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (instantiatedCube != null)
+		{
+			elapsedTime += Time.deltaTime;
+			if (elapsedTime > dieTime)
+			{
+				Destroy(instantiatedCube.gameObject);
+				elapsedTime = 0f;
+			}
+		}
+	}
+
+	public void onTouch()
+	{
+		Debug.Log ("creating glow segment");
+		instantiatedCube = (GameObject)Instantiate (cube);
+		instantiatedCube.renderer.material = glowMask;
+	}
+}
---
+++ Assets/Scripts/SmallIntestine/GlowManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlowManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetButtonDown("Fire1")) 
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			GameObject closestSegment = FindClosestSegment(ray.origin);
+			GlowSegment glowScript = closestSegment.GetComponent<GlowSegment> ();
+			glowScript.onTouch ();
+		}
+	}
+
+	GameObject FindClosestSegment(Vector3 pos) 
+	{
+		GameObject[] segment;
+		segment = GameObject.FindGameObjectsWithTag("glow");
+		GameObject closest = null;
+		float distance = Mathf.Infinity;
+		Vector3 position = pos;
+		Debug.Log ("position of click: " + position);
+
+		foreach (GameObject seg in segment) 
+		{
+			Debug.Log ("Distance : " + distance);
+			Vector3 diff = seg.transform.position - position;
+			Debug.Log (seg.transform.position);
+			float curDistance = diff.sqrMagnitude;
+			Debug.Log("curDist: " + curDistance);
+			if (curDistance < distance) 
+			{
+				closest = seg;
+				Debug.Log("current closest: " + closest.gameObject.name);
+				distance = curDistance;
+			}
+		}
+
+		return closest;
+	}
+}
