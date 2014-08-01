35d86e5e85545e8348d6bb8a767abecf457c74a3
Chantal <sakuya3834@gmail.com>
2014-08-01 15:50:17 -0700

    moved material to resources.load for glow

--- Assets/GlowSegment.cs
+++ Assets/GlowSegment.cs
@@ -6,12 +6,18 @@ public class GlowSegment : MonoBehaviour
 	public GameObject cube;
 	private GameObject instantiatedCube;
 
+	// use to get the name of the current segment to find the correct material
+	private string segmentName;
+	private string segmentCode;
+
 	public float dieTime;
 	private float elapsedTime;
 
 	// Use this for initialization
 	void Start () 
 	{
+		segmentName = transform.gameObject.name;
+		segmentCode = segmentName.Substring (segmentName.Length - 3, 3);
 	}
 	
 	// Update is called once per frame
@@ -33,6 +39,7 @@ public class GlowSegment : MonoBehaviour
 		if (instantiatedCube == null)
 		{
 			instantiatedCube = (GameObject)Instantiate (cube);
+			instantiatedCube.renderer.material = (Material)Resources.Load ("glow/SIGlowMask" + segmentCode, typeof(Material));
 		}
 	}
 }
