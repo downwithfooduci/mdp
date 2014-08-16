0f3078fc12bc43165ca336a7ed3ad864eb02694b
Chantal <sakuya3834@gmail.com>
2014-08-16 03:23:49 -0700

    commented some code

--- Assets/Scripts/Enzyme/ParticleGenerator.cs
+++ Assets/Scripts/Enzyme/ParticleGenerator.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// script used to generate particles in the enzyme game
 public class ParticleGenerator : MonoBehaviour
 {	
+	// variables to hold all the different types of particles that can spawn
 	public GameObject parentCylCyl;
 	public GameObject parentCylCap;
 	public GameObject parentCylSphere;
@@ -20,12 +22,17 @@ public class ParticleGenerator : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
+		// populate the color array with the possible choices
+		// currently the choices are only red, blue, yellow, green, white
 		colors = new Color[5];
 		colors [0] = Color.red;
 		colors [1] = Color.blue;
 		colors [2] = Color.yellow;
 		colors [3] = Color.green;
 		colors [4] = Color.white;
+
+		// populate the parent array
+		// put a reference to each type we can have
 		parents = new GameObject[9];
 		parents[0] = parentCylCyl;
 		parents[1] = parentCylCap;
@@ -36,27 +43,39 @@ public class ParticleGenerator : MonoBehaviour
 		parents[6] = parentCapCap;
 		parents[7] = parentCapCyl;
 		parents[8] = parentCapSphere;
-		
-		for (int i = 0; i<numSpawn; i++) {
+
+		// if the script is hardcoded from the unity editor to spawn a 
+		// certain number of particles on script starting, then do so
+		for (int i = 0; i<numSpawn; i++) 
+		{
 			SpawnParticle();
 		}
 	}
-	
+
+	// function to spawn a new particle
 	public void SpawnParticle ()
 	{
+		// choose a random x and y location to spawn the particle
 		float x = Random.Range(-10.0f, 10.0f);
 		float y = Random.Range(-5.0f, 5.0f);
-		GameObject spawn = (GameObject)Instantiate (parents [Random.Range (0, parents.Length)], new Vector3 (Random.Range (-10f, 10f), Random.Range (-6f, 6f), 0), Quaternion.identity);
+
+		// choose a random type of particle
+		GameObject spawn = (GameObject)Instantiate (parents [Random.Range (0, parents.Length)], 
+		                                            new Vector3 (Random.Range (-10f, 10f), Random.Range (-6f, 6f), 0), 
+		                                            Quaternion.identity);
 		MeshRenderer[] renderers = spawn.GetComponentsInChildren<MeshRenderer> ();
+
+		// choose a random particle color and assign it to all the renderers
 		Color color = colors [Random.Range (0, 5)];
-		for (int j = 0; j < renderers.Length; j++) {
+		for (int j = 0; j < renderers.Length; j++) 
+		{
 			renderers [j].material.color = color;
 		}
+
+		// give the particle an initial impulse
 		spawn.rigidbody.AddForce(new Vector3(x, y, 0), ForceMode.Impulse);
 	}
 	
 	// Update is called once per frame
-	void Update ()
-	{
-	}
+	void Update () {}
 }
--- Assets/Scripts/Enzyme/ParticleSplit.cs
+++ Assets/Scripts/Enzyme/ParticleSplit.cs
@@ -1,31 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ParticleSplit : MonoBehaviour {
+public class ParticleSplit : MonoBehaviour 
+{
+	// variables for the children of a parent and to monitor if it is split
 	public GameObject child1;
 	public GameObject child2;
 	private bool isSplit;
+
 	// Use this for initialization
-	void Start () {
-		isSplit = false;
+	void Start () 
+	{
+		isSplit = false;	// when we begin the particle will not be split
+							// it will need to be split with an appropriate collision
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
-	
+	void Update () {}
+
+	// function to split a particle
 	public void Split()
 	{
-		if(!isSplit)
+		if(!isSplit)	// first make sure it's not already split
 		{
 			isSplit = true;
+
+			// cause the particles to split off from the parent
 			foreach(Transform child in transform)
 			{
 				GameObject toCreate = child.gameObject.name == child1.gameObject.name ? child1 : child2;
 				GameObject created = (GameObject)Instantiate(toCreate, 
 					transform.position,
 					transform.rotation);
+
+				// assign proper color, zero the velocity, and give each split child an impulse force to start moving
 				created.renderer.material.color = child.gameObject.renderer.material.color;
 				created.rigidbody.velocity = Vector3.zero;
 				created.rigidbody.AddForce(created.transform.right * (10 * child.position.x), ForceMode.Impulse);
