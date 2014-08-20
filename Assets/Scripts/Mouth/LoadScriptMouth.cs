affa6615c405be2f1d0fa6f7ceae8cf970a6295b
Chantal <sakuya3834@gmail.com>
2014-08-19 19:37:43 -0700

    code documentation

--- Assets/Scripts/Mouth/LoadScriptMouth.cs
+++ Assets/Scripts/Mouth/LoadScriptMouth.cs
@@ -2,39 +2,45 @@
 using System.Collections;
 using System.IO;
 
+// class that handles loading scripts for the mouth game
 public class LoadScriptMouth 
 {
-	public LoadScriptMouth() {}
-	
+	public LoadScriptMouth() {}		// constructor
+
+	// function that handles the majority of script loading
 	public MouthWave[] loadMouthLevel(int level)
 	{
 		TextAsset lev = Resources.Load ("MouthLevel" + level) as TextAsset;  // for ipad we need to load resources instead of using a file
 		
 		StringReader reader = new StringReader (lev.text);
 		
-		string[] lines = lev.text.Split("\n"[0]);	// split just to count number of lines to create waves array
+		string[] lines = lev.text.Split("\n"[0]);							// split just to count number of lines to create waves array
 		
-		MouthWave[] waves = new MouthWave[lines.Length];
+		MouthWave[] waves = new MouthWave[lines.Length];					
 		
 		string line;	// store the content of the current line
 		int j = 0;  	// index for lines array
+
+		// parse through the file
 		while((line = reader.ReadLine()) != null)
 		{
-			MouthWave wave = new MouthWave();			// object that holds all information about a given wave
+			MouthWave wave = new MouthWave();					// object that holds all information about a given wave
 			
-			string[] waveInfo = line.Split("/"[0]);			// split the current wave line for easier parsing
-			wave.startDelay = float.Parse(waveInfo[0]);
-			wave.runTime = float.Parse(waveInfo[1]);
-			wave.foodSpeed = float.Parse(waveInfo[2]);
-			wave.foodSpawnInterval = float.Parse(waveInfo[3]);
+			string[] waveInfo = line.Split("/"[0]);				// split the current wave line for easier parsing
+			wave.startDelay = float.Parse(waveInfo[0]);			// the first field in the split is the startDelay
+			wave.runTime = float.Parse(waveInfo[1]);			// the second field in the split is the runTime
+			wave.foodSpeed = float.Parse(waveInfo[2]);			// the third field in the split is the foodSpeed
+			wave.foodSpawnInterval = float.Parse(waveInfo[3]);	// the fourth field in the split is the foodSpawnInterval
 			
-			waves[j] = wave;
-			j++;
+			waves[j] = wave;									// store the parsed data in the waves array
+			j++;												// increment the index
 		}
-		return waves;
+
+		return waves;											// return the parsed script data as mouthWaves
 	}
 }
 
+// class that defines a mouth wave
 public class MouthWave
 {
 	public float startDelay;
@@ -42,4 +48,3 @@ public class MouthWave
 	public float foodSpeed;
 	public float foodSpawnInterval;
 }
-
--- Assets/Scripts/SmallIntestine/LoadScript.cs
+++ Assets/Scripts/SmallIntestine/LoadScript.cs
@@ -5,8 +5,7 @@ using System.IO;
 // controls script loading for the small intestine game
 public class LoadScript
 {
-
-	public LoadScript() {}
+	public LoadScript() {}		// constructor
 
 	public SIWave[] loadIntestineLevel(int level)
 	{
@@ -20,20 +19,22 @@ public class LoadScript
 
 		string line;	// store the content of the current line
 		int j = 0;  	// index for lines array
+
+		// parse the script into si waves
 		while((line = reader.ReadLine()) != null)
 		{
 			SIWave wave = new SIWave();			// object that holds all information about a given wave
 
-			string[] waveInfo = line.Split("/"[0]);			// split the current wave line for easier parsing
-			wave.startDelay = float.Parse(waveInfo[0]);
-			wave.runTime = float.Parse(waveInfo[1]);
-			wave.nutrientSpeed = float.Parse(waveInfo[2]);
-			wave.nutrientSpawnInterval = float.Parse(waveInfo[3]);
-			wave.minBlobs = int.Parse(waveInfo[4]);
-			wave.maxBlobs = int.Parse(waveInfo[5]);
+			string[] waveInfo = line.Split("/"[0]);					// split the current wave line for easier parsing
+			wave.startDelay = float.Parse(waveInfo[0]);				// the first field in the line is the startDelay
+			wave.runTime = float.Parse(waveInfo[1]);				// the second field in the line is the runTime
+			wave.nutrientSpeed = float.Parse(waveInfo[2]);			// the third field in the line is the nutrientSpeed
+			wave.nutrientSpawnInterval = float.Parse(waveInfo[3]);	// the fouth field in the line is the nutrientSpawnInterval
+			wave.minBlobs = int.Parse(waveInfo[4]);					// the fifth field in the line is the minBlobs
+			wave.maxBlobs = int.Parse(waveInfo[5]);					// the sixth field in the line is the maxBlobs
 
-			Color[] colors = new Color[(waveInfo[6].Length)];
-
+			Color[] colors = new Color[(waveInfo[6].Length)];		// the seventh field in the line are the colors
+			// the colors need to be parsed to add the actual colors to the array instead of the text codes for them
 			for(int k = 0; k < colors.Length; k++)
 			{
 				switch (waveInfo[6][k])
@@ -50,13 +51,15 @@ public class LoadScript
 				}
 			}
 			wave.colors = colors;
-			waves[j] = wave;
-			j++;
+
+			waves[j] = wave;			// save the wave in the array
+			j++;						// increment the counter
 		}
 		return waves;
 	}
 }
 
+// class that represents the format of an si wave
 public class SIWave
 {
 	public float startDelay;
