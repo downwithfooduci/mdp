5a60c86553d2c817658e9732643702d0e073ee4a
Chantal <sakuya3834@gmail.com>
2014-08-19 20:09:49 -0700

    code documentation

--- Assets/Scripts/SmallIntestine/NutrientManager.cs
+++ Assets/Scripts/SmallIntestine/NutrientManager.cs
@@ -6,29 +6,31 @@ using System.Collections.Generic;
 
 public class NutrientManager : MonoBehaviour 
 {
-    public GameObject NutrientObj;
+    public GameObject NutrientObj;								// hold a nutrient gameobject
 
-    private Dictionary<Color, List<Nutrient>> m_Nutrients;
+    private Dictionary<Color, List<Nutrient>> m_Nutrients;		// creates dictionary with list of all nutrients of each color
 
 	void Awake () 
 	{
-        m_Nutrients = new Dictionary<Color, List<Nutrient>>(new ColorComparer());
+        m_Nutrients = new Dictionary<Color, List<Nutrient>>(new ColorComparer());	// create the dictionary
 	}
 
+	// function that instantiates new nutrients
     public Nutrient InstantiateNutrient(Color color, Vector3 position, Quaternion rotation)
     {
-        GameObject NutrientObject = Instantiate(NutrientObj, position, rotation) as GameObject;
-        Nutrient Nutrient = NutrientObject.GetComponent<Nutrient>();
-        Nutrient.BodyColor = color;
+        GameObject NutrientObject = Instantiate(NutrientObj, position, rotation) as GameObject;	// instantiate a nutrient
+        Nutrient Nutrient = NutrientObject.GetComponent<Nutrient>();	// get the nutrient script from the newly instantiated nutrient
+        Nutrient.BodyColor = color;										// set the color correctly
 
-        if (!m_Nutrients.ContainsKey(color))
+		// keep track of this nutrient in the dictionary
+        if (!m_Nutrients.ContainsKey(color))	// if there is no list for this color in the dictionary, add it
         {
             m_Nutrients.Add(color, new List<Nutrient>());
         }
 
-        m_Nutrients[color].Add(Nutrient);
+        m_Nutrients[color].Add(Nutrient);	// adds the nutrient to the dictionary
 
-        return Nutrient;
+        return Nutrient;	// returns the newly created nutrient
     }
 
     public Nutrient InstantiateNutrient(Color color, Vector3 position)
