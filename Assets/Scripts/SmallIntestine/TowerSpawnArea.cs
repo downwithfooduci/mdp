09b7425a0125f4738d82f33a18be88721adc3f0d
Chantal <sakuya3834@gmail.com>
2014-08-20 18:29:22 -0700

    code documentation

--- Assets/Scripts/SmallIntestine/TowerSpawnArea.cs
+++ Assets/Scripts/SmallIntestine/TowerSpawnArea.cs
@@ -1,23 +1,27 @@
 using UnityEngine;
 using System.Collections;
 
+// script that handles creating the tower spawn
 public class TowerSpawnArea : MonoBehaviour 
 {
-	private TowerSpawner towerSpawner;
+	private TowerSpawner towerSpawner;	// to hold a reference to the tower spawner
 
 	void Start () 
 	{
+		// find a reference to the tower spawner
 		towerSpawner = FindObjectOfType(typeof(TowerSpawner)) as TowerSpawner;
 	}
 
+	// checks if the location where you are dragging a tower over is valid
+	// this will effect whether the green or red circle is shwon around the tower
     void OnMouseOver()
     {
-	    towerSpawner.IsMouseOverWall = true;
-		towerSpawner.wall = gameObject;
+	    towerSpawner.IsMouseOverWall = true;	// set the value if the tower is currently hovering over a wall
+		towerSpawner.wall = gameObject;			// assign the wall in the tower spawner to the one you are hovering over
     }
 
     void OnMouseExit()
     {
-		towerSpawner.IsMouseOverWall = false;
+		towerSpawner.IsMouseOverWall = false;	// when we leave hovering over the wall reset the ismouseoverwall to false
     }
 }
--- Assets/Scripts/SmallIntestine/TowerSpawner.cs
+++ Assets/Scripts/SmallIntestine/TowerSpawner.cs
@@ -1,84 +1,96 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+// script that handles most of the basic functionality of the tower spawner
 public class TowerSpawner : MonoBehaviour
 {
 	// Randomly selects a tower model upon creation
-	public GameObject[] Towers;
-	public GameObject SpawnIndicator;
-	DebugConfig debugConfig;
-
-	// texture for bottom bar area
-	public Texture bottomBar;
+	public GameObject[] Towers;				// an array to store towers
+	public GameObject SpawnIndicator;		// creates the spawn indicator (the red or green circle around the tower)
+	DebugConfig debugConfig;				// to hold a reference to the debugconfig script
 
 	// Set available tower colors in editor
-	public Color[] AvailableColors;
-	public GUIStyle ButtonStyle;
+	public Color[] AvailableColors;			// an array that holds a list of possible tower colors
 
 	// for sounds
-	public GameObject placementSound;
-	
-	public Rect Dimensions;
-	public bool IsMouseOverWall;
-	public GameObject wall;
+	public GameObject placementSound;		// to hold the sound that will be played when a tower is placed
+
+	public bool IsMouseOverWall;			// a flag that is set to indicate the tower is currently over a wall
+	public GameObject wall;					// a reference to the wall that the tower is hovering over
 
 	// Need to poll from last frame, otherwise
 	// will return false as soon as user releases mouse
-	private bool m_IsMouseOverWallLastFrame;
-	private List<GameObject> m_SpawnAreas;
-	private Rect m_ButtonSize;
-	private bool m_IsSpawnActive;
-	private GameObject m_SpawnedTower;
-	private GameObject m_Indicator;
-	private IntestineGameManager m_GameManager;
-	public int TOWER_BASE_COST = 20;
-	private const float timer = 2.0f;
+	private bool m_IsMouseOverWallLastFrame;		// to hold a flag to indicate if the mouse was over a wall in the last update
+	private bool m_IsSpawnActive;					// to flag whether or not the spawn is active
+
+	private GameObject m_SpawnedTower;				// a reference to the current spawned tower
+	private GameObject m_Indicator;					// the gameobject that creates the spawn indicator (red or green circle)
+	private IntestineGameManager m_GameManager;		// holds a reference to the game manager
+
+	public int TOWER_BASE_COST = 20;				// the tower base placement cost
+	private const float timer = 2.0f;				// a timer
 		
 	void Start ()
 	{
 		// make sure we aren't in tutorial
 		if (Application.loadedLevelName != "SmallIntestineTutorial")
 		{
+			// if we aren't in the tutorial get a reference to the debugger
 			debugConfig = ((GameObject)GameObject.Find("Debug Config")).GetComponent<DebugConfig>();
-			TOWER_BASE_COST = debugConfig.TOWER_BASE_COST;
-		}
-		m_IsSpawnActive = false;
 
+			// if we are using the debugger values get the tower base cost from there
+			if (debugConfig.debugActive)
+			{
+				TOWER_BASE_COST = debugConfig.TOWER_BASE_COST;
+			}
+		}
+
+		m_IsSpawnActive = false;		// the spawn active should default to false
+
+		// find a reference to the current intestine game manager
 		m_GameManager = GameObject.Find ("Managers").GetComponent<IntestineGameManager> ();
 	}
 
 	void Update ()
 	{
+		// check if the game is paused we destroy any spawned tower that isn't currently placed
 		if(Time.timeScale <= 0.001f)
 		{
+			// this handles the cleanup of variables that need to be reassigned based on destroying unplaced tower
 			if(m_IsSpawnActive)
 			{
-				m_IsSpawnActive = false;
-				DestroyImmediate (m_Indicator.renderer.material);
-				Destroy (m_Indicator);
-				Destroy (m_SpawnedTower);
-				m_SpawnedTower = null;
+				m_IsSpawnActive = false;							// set that isspawnactive flag to false
+				DestroyImmediate (m_Indicator.renderer.material);	// destroy the indicator material
+				Destroy (m_Indicator);								// destroy the indicator
+				Destroy (m_SpawnedTower);							// destroy the spawned tower
+				m_SpawnedTower = null;								// set the spawnedtower reference to null
 			}
 		}
 
+		// if we are not in the tutorial and we are using the debugger, check for updates to the tower base cost
 		if (Application.loadedLevelName != "SmallIntestineTutorial")
 		{
-			TOWER_BASE_COST = debugConfig.TOWER_BASE_COST;
+			if (debugConfig.debugActive)
+			{
+				TOWER_BASE_COST = debugConfig.TOWER_BASE_COST;
+			}
 		}
 
 		// Handle valid spawn locations if player is spawning a tower
 		if (m_IsSpawnActive) 
 		{
+			// check that the mouse button is not currently pressed
 			if (Input.GetMouseButtonUp (0)) 
 			{
-				m_IsSpawnActive = false;
+				m_IsSpawnActive = false;				// set the spawn active flag to false
 
-				DestroyImmediate (m_Indicator.renderer.material);
-				Destroy (m_Indicator);
+				DestroyImmediate (m_Indicator.renderer.material);	// destroy the spawn indicator material
+				Destroy (m_Indicator);								// destroy the spawn indicator
 			
+				// check if the user has enough nutrients to spawn a tower
 				if (m_IsMouseOverWallLastFrame && m_GameManager.nutrients - TOWER_BASE_COST >= 0) 
 				{
-					
+					// if they do have enough nutrients
 					m_SpawnedTower.GetComponent<Tower> ().enabled = true;
 					m_SpawnedTower.transform.position = wall.transform.position + new Vector3 (0, 0.5f, 0);
 					m_SpawnedTower.GetComponent<Tower>().wall = wall;
@@ -127,19 +139,21 @@ public class TowerSpawner : MonoBehaviour
 		m_IsSpawnActive = true;
 	}
 
+	// function that can be called to determine if the spawn is currently active or not
 	public bool getIsSpawnActive()
 	{
 		return m_IsSpawnActive;
 	}
 
+	// function that can be called that will return a reference to the currently spawned tower
 	public GameObject getSpawnedTower()
 	{
 		return m_SpawnedTower;
 	}
 
+	// function that can be called to return the gamemanager instance stored in this script
 	public IntestineGameManager getGameManager()
 	{
 		return m_GameManager;
 	}
 }
-
