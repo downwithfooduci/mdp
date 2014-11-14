2615e0706acd790e505a9789f501f99c10d66b5c
sakuya3834@gmail.com <sakuya3834@gmail.com>
2014-11-14 01:06:11 -0800

    food now comes into the stomach

---
+++ Assets/Scripts/Stomach/StomachFoodBlob.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StomachFoodBlob : MonoBehaviour 
+{
+	public Texture[] wholeFood;
+	public Texture[] digestedFood;
+
+	private Texture wholeRepresentation;
+	private Texture digestedRepresentation;
+
+	private float spawnLocation;
+	private float yLocation = 0f;
+
+	int index = 6;
+
+	// Use this for initialization
+	void Start () 
+	{
+		index = Random.Range (0, 4);
+		Debug.Log (index);
+		wholeRepresentation = wholeFood [index];
+		digestedRepresentation = digestedFood [index];
+
+		spawnLocation = Random.Range (.4f * Screen.width, .85f * Screen.width);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (yLocation < .9f * Screen.height)
+		{
+			yLocation += Time.deltaTime * 50;
+		}
+	}
+
+	void OnGUI()
+	{
+		GUI.depth -= 30;
+		GUI.DrawTexture (new Rect (spawnLocation, Mathf.Min(yLocation, .75f * Screen.height), 
+		                           .1875f * Screen.width, .255f * Screen.height), wholeRepresentation);
+	}
+}
---
+++ Assets/Scripts/Stomach/StomachFoodManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StomachFoodManager : MonoBehaviour 
+{
+	public GameObject stomachFoodBlob;
+	private List<StomachFoodBlob> spawnedFoodBlobs = new List<StomachFoodBlob>();
+
+	public float timeBetweenFoodSpawns;
+	private float elapsedTime;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		elapsedTime += Time.deltaTime;
+
+		if (elapsedTime > timeBetweenFoodSpawns)
+		{
+			GameObject temp = (GameObject)Instantiate (stomachFoodBlob);
+			spawnedFoodBlobs.Add(stomachFoodBlob.GetComponent<StomachFoodBlob>());
+			elapsedTime = 0f;
+		}
+	}
+}
