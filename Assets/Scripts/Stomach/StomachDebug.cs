4de927a50fa631df5e45ec4ec9364767c81e61e7
sakuya3834@gmail.com <sakuya3834@gmail.com>
2014-12-18 23:20:06 -0800

    making a test build

--- Assets/Scripts/Stomach/StomachDebug.cs
+++ Assets/Scripts/Stomach/StomachDebug.cs
@@ -1,7 +1,41 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
-public class StomachDebug : MonoBehaviour {
+public class StomachDebug : MonoBehaviour 
+{
+	/**
+	 * Debugger items to pull values from
+	 */
+	public UnityEngine.UI.InputField maxFoodInput;
+	public UnityEngine.UI.InputField timeToBurnInput;
+	public UnityEngine.UI.InputField timeForSlimeDecayInput;
+	public UnityEngine.UI.InputField timeToReviveInput;
+	public UnityEngine.UI.InputField timeForBurnNoSlimeInput;
+	public UnityEngine.UI.InputField maxCellDeathsInput;
+	public UnityEngine.UI.InputField acidityThresholdInput;
+	public UnityEngine.UI.InputField basicThresholdInput;
+	public UnityEngine.UI.InputField acidSpeedInput;
+	public UnityEngine.UI.InputField baseSpeedInput;
+	public UnityEngine.UI.InputField idleDecaySpeedInput;
+	public UnityEngine.UI.Toggle useDebuggerToggle;
+
+	/**
+	 * Actual variables holding the values we are using
+	 * parsed from the debugger
+	 */
+	private int maxFood;
+	private float timeToBurnFromNorm;
+	private float timeForSlimeDecay;
+	private float timeToRevive;
+	private float timeToBurnWithSlime;
+	private int maxCellDeaths;
+	private int acidity;
+	private int basic;
+	private float acidGainSpeed;
+	private float baseGainSpeed;
+	private float idleDecaySpeed;
+	private bool useDebugger;
 
 	// Use this for initialization
 	void Start () {
@@ -11,5 +45,16 @@ public class StomachDebug : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void onSubmit()
+	{
+		useDebugger = useDebuggerToggle.isOn;
+
+		if (useDebugger)
+		{
+			maxFood = int.Parse(maxFoodInput.GetComponent<Text>().text);
+			timeToBurnFromNorm = float.Parse(timeForBurnNoSlimeInput.GetComponent<Text>().text);
+		}
 	}
 }
--- Assets/Scripts/Stomach/StomachDebugGUI.cs
+++ Assets/Scripts/Stomach/StomachDebugGUI.cs
@@ -1,24 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+ * Class with basic scripting functions to control the 
+ * behavior of the debug gui in the stomach game
+ */
 public class StomachDebugGUI : MonoBehaviour 
 {
-	public Canvas ui;
-	public RectTransform r;
+	public Canvas ui;			//!< the actual ui canvas
+	public RectTransform r;		//!< the rect transform to change the size and scale of the ui
 
 	// Use this for initialization
 	void Start () 
 	{
 		ui.enabled = false;
-
-
 	}
 
+	/**
+	 * When the debug ui is enabled, pause the game and set 
+	 * the values required for it to render properly
+	 */
 	public void enable()
 	{
 		Time.timeScale = 0f;
 		ui.enabled = true;
 		r.anchoredPosition = new Vector2 (1023f/2f, 768f/2f);
 		r.localScale = new Vector3 (1f, 1f, 1f);
+	}
+
+	/**
+	 * When the debug ui is disabled, unpause the game
+	 */
+	public void disable()
+	{
+		Time.timeScale = 1f;
+		ui.enabled = false;
 	}
 }
