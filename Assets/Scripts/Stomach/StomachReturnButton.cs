47f2a73a5e030cd78b862d136647103322bb1f35
sakuya3834@gmail.com <sakuya3834@gmail.com>
2014-12-19 00:58:06 -0800

    set up gui anchoring

--- Assets/Scripts/Stomach/StomachReturnButton.cs
+++ Assets/Scripts/Stomach/StomachReturnButton.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class StomachReturnButton : MonoBehaviour 
 {
 	public Canvas ui;
+	public RectTransform r;		//!< the rect transform to change the size and scale of the ui
+	public RectTransform yes;
+	public RectTransform no;
+
 
 	void Start()
 	{
@@ -14,6 +19,9 @@ public class StomachReturnButton : MonoBehaviour
 	{
 		Time.timeScale = 0f;
 		ui.enabled = true;
+		r.localScale = new Vector3 (1f, 1f, 1f);
+		yes.localScale = new Vector3 (1f, 1f, 1f);
+		no.localScale = new Vector3 (1f, 1f, 1f);
 	}
 
 	public void returnToMain()
