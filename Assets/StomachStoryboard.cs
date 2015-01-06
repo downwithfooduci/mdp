d3965c893b5ff95db46208f8ed3d208fdde79a2a
sakuya3834@gmail.com <sakuya3834@gmail.com>
2015-01-06 10:29:33 -0800

    added a placeholder intro to stomach game

---
+++ Assets/StomachStoryboard.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// PLACEHOLDER!!!
+public class StomachStoryboard : MonoBehaviour 
+{
+
+	DetectStraightSwipe swipeDetection;	//!< to hold a reference to the script that controls swipe detection
+
+	// Use this for initialization
+	void Start () 
+	{
+		// find the script for detecting touch
+		swipeDetection = gameObject.GetComponent<DetectStraightSwipe> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (swipeDetection.getSwipeLeft())		// attempt to detect a swipe to the right
+		{
+			Application.LoadLevel ("Stomach");
+		}
+	}
+}
