acbfe0602f00ae25625f2f15b8c254bc8674ec74
sakuya3834@gmail.com <sakuya3834@gmail.com>
2015-01-10 23:38:01 -0800

    screen scaling improvement progress in stomach game

---
+++ Assets/Scripts/Stomach/DisableMainGUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DisableMainGUI : MonoBehaviour 
+{
+	public Canvas c;
+	public UnityEngine.UI.Button debug;
+
+	public void disable()
+	{
+		c.enabled = false;
+		debug.enabled = false;
+	}
+
+	public void enable()
+	{
+		c.enabled = true;
+		debug.enabled = true;
+	}
+}
