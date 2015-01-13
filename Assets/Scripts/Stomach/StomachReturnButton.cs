fba17a9012deca226600376a7c64975d0dd1cc80
sakuya3834@gmail.com <sakuya3834@gmail.com>
2015-01-12 21:05:17 -0800

    fixed quit game popup

--- Assets/Scripts/Stomach/StomachReturnButton.cs
+++ Assets/Scripts/Stomach/StomachReturnButton.cs
@@ -5,10 +5,7 @@ using System.Collections;
 public class StomachReturnButton : MonoBehaviour 
 {
 	public Canvas ui;
-	public RectTransform r;		//!< the rect transform to change the size and scale of the ui
-	public RectTransform yes;
-	public RectTransform no;
-
+	public Canvas main;
 
 	void Start()
 	{
@@ -19,9 +16,7 @@ public class StomachReturnButton : MonoBehaviour
 	{
 		Time.timeScale = 0f;
 		ui.enabled = true;
-		r.localScale = new Vector3 (1f, 1f, 1f);
-		yes.localScale = new Vector3 (1f, 1f, 1f);
-		no.localScale = new Vector3 (1f, 1f, 1f);
+		main.enabled = false;
 	}
 
 	public void returnToMain()
@@ -33,5 +28,6 @@ public class StomachReturnButton : MonoBehaviour
 	{
 		Time.timeScale = 1f;
 		ui.enabled = false;
+		main.enabled = true;
 	}
 }
