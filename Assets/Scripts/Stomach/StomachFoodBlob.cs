b036e5d4c79ec269528b63e1a6d0222e5dec23a4
sakuya3834@gmail.com <sakuya3834@gmail.com>
2015-01-22 13:37:50 -0800

    improved stomach food hitting invisible walls

--- Assets/Scripts/Stomach/StomachFoodBlob.cs
+++ Assets/Scripts/Stomach/StomachFoodBlob.cs
@@ -21,7 +21,7 @@ public class StomachFoodBlob : MonoBehaviour
 		wholeRepresentation = wholeFood [index];
 		digestedRepresentation = digestedFood [index];
 
-		spawnLocation = Random.Range (.5f * Screen.width, .85f * Screen.width);
+		spawnLocation = Random.Range (.6f * Screen.width, .85f * Screen.width);
 		parent.transform.position = new Vector3 ((spawnLocation * 15f / Screen.width) - 7.5f, 
 		                                         11f - (0f * 11f / Screen.height) - 5.5f, -2.0f);
 		GetComponent<SpriteRenderer>().sprite = wholeRepresentation;
