8e98bbacabea45dd2a474e12a9f26b41086685ac
Iris Zeng <chenjunz@uci.edu>
2016-01-05 11:30:08 -0800

    Modification on Large Intestine game's background scrolling and subtractors

--- Assets/Large Intestine Assert/backgroundMove.cs
+++ Assets/Large Intestine Assert/backgroundMove.cs
@@ -8,15 +8,18 @@ public class backgroundMove : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
-
+//		Debug.Log (rb.position.x);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		x = rb.position.x;
-		if (x > (182.5f-30.8425f))
+//		Debug.Log (rb.position);
+		if (x > 239.4f)
 			rb.velocity = new Vector2 (-3, 0);
-		else
-			rb.position = new Vector2 ((182.5f+30.8425f), 137f);
+		else {
+			rb.position = new Vector2(rb.position.x+30.715f*2,rb.position.y);
+			Debug.Log (rb.position);
+		}
 	}
 }
