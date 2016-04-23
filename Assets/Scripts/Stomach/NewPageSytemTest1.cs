29c38a054902f4d5daedd5c97a4417de0772c0fc
richardshi <shiq2@uci.edu>
2016-04-22 20:48:05 -0700

    4.21.2016 StoryBroad Test1
    Minor Test New System

---
+++ Assets/Scripts/Stomach/NewPageSytemTest1.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NewPageSytemTest1 : MonoBehaviour {
+
+	public int size;
+	public Texture[] PageImage;	
+
+	private List<Pages> PageList = new List<Pages>();
+
+
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i<PageImage.Length; i++){
+			PageList.Add (new Pages (PageImage [i]));
+		}
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Number of Texture:" + PageList.Count);
+	
+	}
+}
+
+public class Pages : NewPageSytemTest1
+{
+	public Texture PageTextture;
+	public Pages(Texture pageimage){
+		PageTextture = pageimage;
+	}
+	public void setPage(Texture pageText){
+		this.PageTextture = pageText;
+
+	}
+
+}
