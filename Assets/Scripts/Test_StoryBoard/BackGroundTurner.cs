a95ed48ac00e5ea9bfae0ffe0f739eebd45c7873
Richard Shi <shiq2@uci.edu>
2016-05-17 22:25:40 -0700

    05/17/2016
    StoryBoard:
    Sprite version succeeded.
    
    To do:
    Implement it into the system.

---
+++ Assets/Scripts/Test_StoryBoard/BackGroundTurner.cs
@@ -0,0 +1,227 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BackGroundTurner : MonoBehaviour {
+
+	/***************************************************************/
+	public AudioClip[] sounds;			//!< store the storyboard narrations
+	private int currPage = 1;			//!< store the current page
+	private bool hasPlayed = false;		//!< remember whether the current sound has played
+	private bool buttonClicked = false;	//!< holds if the user clicked in the page corner instead of swiping
+
+	DetectStraightSwipe swipeDetection;	//!< to hold a reference to the script that controls swipe detection
+
+	private bool canSkip = false;		//!< check for playthrough
+
+	private AsyncOperation loader;		//!< for preloading the next level
+
+	/***************************************************************/
+
+
+
+
+
+
+
+
+	public Sprite[] BackGroundImages;
+	private Image image;
+
+	public int[] CharacterPage;
+	public float[] XCharacterPosition;
+	public float[] YCharacterPosition;
+	public Sprite[] CharacterImage;
+
+
+
+	public List<Pages_Sprites> PageList = new List<Pages_Sprites>();
+	private HashSet<int> charSet = new HashSet<int>();
+
+
+
+	private StoryCharacter storyChar;
+	private bool charAdded;
+	private int charTracker;
+
+
+	// Use this for initialization
+	void Start () {
+
+		image = GetComponent<Image>();
+		storyChar = FindObjectOfType (typeof(StoryCharacter)) as StoryCharacter;
+
+		charAdded = false;
+		charTracker = 0;
+
+
+
+		for (int i = 0; i < CharacterPage.Length; i++) {
+			charSet.Add (CharacterPage [i]);
+			Debug.Log ("Page " + CharacterPage[i] + " added");
+		}
+
+		int tempCharCounter = 0;
+		for (int i = 0; i < BackGroundImages.Length; i++) {
+			PageList.Add (new Pages_Sprites (BackGroundImages [i], i));
+			if (charSet.Contains (i)) {
+				Sprite[] tempCharSprite = new Sprite[2];
+
+				tempCharSprite [0] = CharacterImage [tempCharCounter*2];
+				tempCharSprite [1] = CharacterImage [tempCharCounter*2 + 1];
+
+				PageList [i].setCharacter (tempCharSprite, XCharacterPosition [tempCharCounter], YCharacterPosition [tempCharCounter]);
+				tempCharCounter++;
+			}
+		}
+
+
+
+
+		/**************************************************************/
+		swipeDetection = gameObject.GetComponent<DetectStraightSwipe> ();
+
+
+		/**************************************************************/
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+
+
+		/**************************************************************/
+
+		if (!GetComponent<AudioSource>().isPlaying || canSkip)
+		{
+			if (swipeDetection.getSwipeLeft() || buttonClicked)		// attempt to detect a swipe to the right
+			{
+				buttonClicked = false;
+				swipeDetection.resetSwipe();						// reset the variables to prevent multiple page turns
+				currPage++;											// increment the page since we are going forward
+				hasPlayed = false;
+				charAdded = false;
+				
+			} else if (swipeDetection.getSwipeRight() == true)			// attempt to detect a swipe to the left
+			{
+				swipeDetection.resetSwipe();						// reset the varaibel to prevent multiple page turns
+
+				if (currPage - 1 > 0)								// perform bounds checking to make sure we don't go back too far
+				{
+					currPage--;
+					hasPlayed = false;
+					charAdded = false;
+
+
+				}
+			}
+		} else if (GetComponent<AudioSource>().isPlaying)
+		{
+			swipeDetection.resetSwipe();							// if we can't change the page yet forget the swipe
+		}
+
+		if (!hasPlayed)								// only play the clip once per page
+		{
+			if (!((currPage - 1) >= PageList.Count))
+			{
+				GetComponent<AudioSource>().clip = sounds [currPage - 1];		// if we haven't played the sound yet load the new audio clip
+				playClip();								// play the clip
+				hasPlayed = true;						// mark that we have played the clip
+			}
+		}
+
+		/**************************************************************/
+
+
+		int tempPageNum = Mathf.Clamp (currPage - 1, 0, BackGroundImages.Length - 1);
+		image.sprite = BackGroundImages [tempPageNum];
+
+		if (charSet.Contains (tempPageNum)) {
+			//Debug.Log ("Current Page: " + tempPageNum);
+			storyChar.setcharOn ();
+		} else {
+			storyChar.setcharOff ();
+		}
+		
+
+
+	
+	}
+
+
+
+	private void playClip()
+	{
+		GetComponent<AudioSource>().Play();
+	}
+
+	public int currentPage(){
+		return Mathf.Clamp (currPage - 1, 0, BackGroundImages.Length - 1);
+	}
+	/*
+	public Vector2 charPosition(){
+		if (charSet.Contains(currentPage())) {
+			return new Vector2(PageList [currentPage ()].PageCharacter.posx, PageList[currentPage()].PageCharacter.posy);
+		}
+		else
+			return new Vector2(0,0);
+	}
+	*/
+
+}
+
+
+public class Pages_Sprites //: NewPageSytemTest1
+{
+	public int num;
+	public Sprite PageTexture;
+	//public Texture[] CharacterTexture;
+	public extraCharacters_Sprites PageCharacter;
+	//public bool hasChar = false;
+	public Pages_Sprites(Sprite pageimage, int n){
+		PageTexture = pageimage;
+		num = n;
+	}
+	public void setPage(Sprite pageText){
+		this.PageTexture = pageText;
+
+	}
+	//public void setCharacter(Texture[] charText, float x, float y){
+	public void setCharacter(Sprite[] charText, float x, float y){
+
+		//CharacterTexture = charText;
+		PageCharacter = new extraCharacters_Sprites(charText, x, y);
+		//hasChar = true;
+	}
+	public Sprite getTexture(){
+		return PageTexture;
+	}
+		
+}
+
+public class extraCharacters_Sprites //: NewPageSytemTest1
+{
+	//public Texture[] charTexture;
+	public Sprite[] charSprite;
+	public float posx;
+	public float posy;
+	public float width;
+	public float height;
+	//public extraCharacters(Texture[] charImage, float x, float y){
+	public extraCharacters_Sprites(Sprite[] charImage, float x, float y){
+		//charTexture = charImage;
+		charSprite = charImage;
+		posx = x;
+		posy = y;
+		width = charImage[0].bounds.size.x;
+		height = charImage [0].bounds.size.y;
+	}
+
+
+}
---
+++ Assets/Scripts/Test_StoryBoard/StoryCharacter.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class StoryCharacter : MonoBehaviour {
+
+
+	public Sprite[] CharacterImages;
+	public Sprite TransImage;
+	private Image image;
+	private BackGroundTurner bgt;
+	private bool charOn = false;
+	private BoxCollider2D charBox;
+
+	private int charTracker;
+
+
+	// Use this for initialization
+	void Start () {
+		image = GetComponent<Image> ();
+		charBox = GetComponent<BoxCollider2D> ();
+		bgt = FindObjectOfType (typeof(BackGroundTurner)) as BackGroundTurner;
+
+
+		charTracker = 0;
+
+		if (bgt.CharacterPage.Length != 0) {
+			CharacterImages = bgt.CharacterImage;
+		}
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(charOn){
+			//transform.position = bgt
+			image.sprite = bgt.PageList [bgt.currentPage ()].PageCharacter.charSprite[charTracker];
+			/*
+			transform.position = new Vector3 (
+				bgt.PageList [bgt.currentPage ()].PageCharacter.posx,
+				bgt.PageList [bgt.currentPage ()].PageCharacter.posy,0f);
+				*/
+
+		} else {
+			image.sprite = TransImage;
+		}
+	
+	}
+
+	public void setcharOn(){
+		charOn = true;
+	}
+
+	public void setcharOff(){
+		charOn = false;
+	}
+
+
+	void OnMouseDown(){
+		Debug.Log("on mouse down");
+		Debug.Log ("Current Page:" + bgt.currentPage ());
+
+	}
+
+	void OnMouseUp(){
+		Debug.Log ("on mouse up");
+		if (charTracker == 0)
+			charTracker = 1;
+		else
+			charTracker = 0;
+
+	}
+
+
+}
