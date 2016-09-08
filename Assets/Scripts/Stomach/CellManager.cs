26e4742eec4fea9190fd59dc9624f5d7f5dcce3d
Richard Shi <shiq2@uci.edu>
2016-09-07 18:03:16 -0700

    09/07/2016 Update
    Stomach:
    New finger animation added:
    Show draging finger while cells burning, unless some cells other than
    cell4(which is right beneath the bucket) is slimed.

--- Assets/Scripts/Stomach/CellManager.cs
+++ Assets/Scripts/Stomach/CellManager.cs
@@ -13,9 +13,12 @@ public class CellManager : MonoBehaviour
     public StomachCell[] cellScripts;		//!< for references to all the stomach cell scripts
 	public CellButtons cellButtons;			//!< for a reference to the cell buttons script
 
-	public Texture tapFigure;				//!< image of tap fingure 
+	public Texture[] tapFigure;				//!< image of tap fingure 
 	private bool bucketPopUp;						//!< boolean value to check if the pop up should appear
 	private int cellNumber;					//!< cell number of first burned cell, -1 if no one burns
+
+	private float animCounter;
+	public float flameTime;
 
 
 	/**
@@ -35,6 +38,7 @@ public class CellManager : MonoBehaviour
 		//initialize the values for pop up
 		bucketPopUp = false;
 		cellNumber = -1;
+		animCounter = 0;
 	}
 
 	void Update()
@@ -42,13 +46,15 @@ public class CellManager : MonoBehaviour
 		//handle popup when the stomach wall is burning
 		for(int i = 0; i < 6; i++)
 		{
-			StomachCell cell = cellScripts [i];
-			if ((cell.getCellState () == "burning") && (cellNumber == -1)) {
-				bucketPopUp = true;
-				cellNumber = i; 
+			if (i != 3) {
+				StomachCell cell = cellScripts [i];
+				if ((cell.getCellState () == "burning") && (cellNumber == -1)) {
+					bucketPopUp = true;
+					cellNumber = i; 
+				}
+				if (cell.getCellState () == "slimed")
+					bucketPopUp = false;
 			}
-			if (cell.getCellState () == "slimed")
-				bucketPopUp = false;
 		}
 	}
 
@@ -57,10 +63,21 @@ public class CellManager : MonoBehaviour
 	{
 		if(bucketPopUp)
 		{
+			animCounter = animCounter + Time.deltaTime;
+			if (animCounter >= flameTime * tapFigure.Length)
+				animCounter = 0;
+			
+			GUI.DrawTexture (new Rect(Screen.width * 0.125f, 
+				Screen.height * 0.45f,//0f,
+				Screen.width * 0.5576f, 
+				Screen.height * 0.5143f),tapFigure[(int)(animCounter/flameTime)]);
+
+			/*
 			GUI.DrawTexture (new Rect(Screen.width * 0.125f, 
 				Screen.height * 0.715f, 
 				Screen.width * 0.2093359375f, 
 				Screen.height * 0.300697917f),tapFigure);
+			*/
 		}
 	}
 	
